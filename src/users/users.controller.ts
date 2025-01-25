d128cb08ede1e28109ed2d2dd0853aeab9bb5ff6
bigosofts <abdullah.limonbau@gmail.com>
2025-01-26 00:21:13 +0600

    Using Decorator for Request and Response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,42 @@
-import { Controller, Get, Post, Patch, Put, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Patch,
+  Put,
+  Delete,
+  Param,
+  Query,
+  Body,
+  Req,
+  Headers,
+  Ip,
+} from '@nestjs/common';
+
+import { Request } from 'express';
 
 @Controller('users')
 export class UsersController {
   @Get()
-  public getUsers() {
-    return 'I am limon from get request';
+  public getAllUsers() {
+    return 'Fetching all users';
+  }
+
+  @Get('/:id/:nid')
+  public getUsers(@Param('id') param: string, @Query('name') query: string) {
+    console.log(param);
+    console.log(query);
+    return `Fetching user with id: ${param}`;
   }
   @Post()
-  public createUsers() {
+  public createUsers(
+    @Body() body: any,
+    @Headers() headers: any,
+    @Ip() ip: any,
+  ) {
+    console.log(body);
+    console.log(headers);
+    console.log(ip);
     return 'I am limon from post request';
   }
 }
